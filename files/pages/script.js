4b367e85f272d32bd3759aef9b1df4bcf981bc1a
vishnumorapally <vishnumorapally2004@gmail.com>
2024-09-23 17:34:34 +0530

    second commit

---
+++ files/pages/script.js
@@ -0,0 +1,40 @@
+function showlistaraku(){
+    var div = document.querySelector(".arakushowlist")
+    if(div.style.display === "none"){
+        div.style.display = "block"
+    }
+    else{
+        div.style.display = "none"
+    }
+}
+
+function showlistamaravathi(){
+
+    var sdiv = document.querySelector(".Amaravathishowlist")
+    if(sdiv.style.display === "none"){
+        sdiv.style.display = "block"
+    }
+    else{
+        sdiv.style.display = "none"
+    }
+}
+
+function showlistVisakhapatnam(){
+    var sdiv = document.querySelector(".Visakhapatnamshowlist")
+    if(sdiv.style.display === "none"){
+        sdiv.style.display = "block"
+    }
+    else{
+        sdiv.style.display = "none"
+    }
+}
+
+function showlistVijayawada(){
+    var sdiv = document.querySelector(".Vijayawadashowlist")
+    if(sdiv.style.display === "none"){
+        sdiv.style.display = "block"
+    }
+    else{
+        sdiv.style.display = "none"
+    }
+}
